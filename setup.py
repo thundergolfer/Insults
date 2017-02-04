549b23cedb0aea94d9349bdbf84090c8d7df27cb
Jono Belotti <jbelotti@zendesk.com>
2017-02-04 16:21:31 +1100

    Adding stuff to get non .py files in pip install

--- setup.py
+++ setup.py
@@ -11,15 +11,27 @@ def get_pip_dependencies():
 
 setup(
   name = 'insults',
+
   packages = find_packages(exclude=['scripts', 'docs', 'tests*']),
+
   version = '0.1.9',
+
   description = 'Identify insulting comments and users on social media',
+
   author = 'Jonathon Belotti',
+
   author_email = 'jonathon.bel.melbourne@gmail.com',
+
   url = 'https://github.com/thundergolfer/Insults', # use the URL to the github repo
+
   download_url = 'https://github.com/thundergolfer/Insult/tarball/0.1', # I'll explain this in a second
+
   keywords = ['machine-learning', 'social-media', 'community', 'data-science'], # arbitrary keywords
+
+
   classifiers = [],
+
+
   install_requires = [
         'pyparsing==1.5.6',
         'python-dateutil==2.4.1',
@@ -32,6 +44,42 @@ setup(
         'futures==2.2.0',
         'nltk==3.0'
   ],
+
+  # If there are data files included in your packages that need to be
+  # installed in site-packages, specify them here.  If using Python 2.6 or less, then these
+  # have to be included in MANIFEST.in as well.
+  include_package_data=True,
+
+  # relative to the vfclust directory
+  package_data={
+    'Data/Estimates':
+            ['Data/Estimates/estimates.csv'],
+        'Data/Final':
+            ['Data/Final/README.md',
+             'Data/Final/final1.csv',
+             'Data/Final/final2.csv',
+             'Data/Final/final8.csv',
+             'Data/Final/final9.csv',
+             'Data/Final/final10.csv'
+            ],
+        'Data/Inputs':
+            ['Data/Inputs/final.csv',
+             'Data/Inputs/fulltrain.csv',
+             'Data/Inputs/sample_submission_null.csv',
+             'Data/Inputs/test.csv',
+             'Data/Inputs/test_with_solutions.csv',
+             'Data/Inputs/train.csv'
+            ],
+        'Data/Submissions':
+            ['Data/Submissions/submission1.csv',
+             'Data/Submissions/submission2.csv',
+             'Data/Submissions/submission3.csv'
+            ],
+        'models':
+            ['models/insult_classifier.joblib.pkl']
+    },
+
+
   cmdclass = {'unit': UnitTest }
 )
 
