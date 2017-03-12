0cc7939d3f557b0bdeaea207d7e40ee2b580ded7
Jonathon Belotti <jbelotti@zendesk.com>
2017-03-12 16:31:54 +1100

    Adding sexist terms list. It's a bit shit right now

---
+++ insults/word_lists/sexist_words.py
@@ -0,0 +1,16 @@
+# IMPORTANT NOTE:
+# This kind of 'word policing' is I think a contentious idea, and many people maybe
+# find the inclusion of some words here inappropiate. Still, I wanted to have this list
+# so that I could do *something* to detect sexist language, as hokey as my current approach is.
+
+sexist_list = set([
+'cunt',
+'grass-eater', # contentious?
+'hussy',
+'mansplainer',
+'slut',
+'tart',
+'whore'
+])
+
+# TODO: There should be more terms than this.
