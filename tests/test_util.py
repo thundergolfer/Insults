0a259ba003290e13da0f6be161ec758b38c63c6d
Jonathon Belotti <jbelotti@zendesk.com>
2017-03-26 22:47:37 +1100

    fix dodgy tests data

--- tests/test_util.py
+++ tests/test_util.py
@@ -9,15 +9,15 @@ def test_data_file():
     path = util.data_file("test_category")
     path_two = util.data_file("test_category", name="test_name")
 
-    assert path == 'Data/test_category'
-    assert path_two == 'Data/test_category/test_name'
+    assert path == 'insults/Data/test_category'
+    assert path_two == 'insults/Data/test_category/test_name'
 
 def test_data_directory():
     path = util.data_file("test_category")
 
-    assert path == 'Data/test_category'
+    assert path == 'insults/Data/test_category'
 
 def test_log_file():
     log_f = util.log_file("test_name")
 
-    assert log_f == 'Logs/test_name'
+    assert log_f == 'insults/Logs/test_name'
