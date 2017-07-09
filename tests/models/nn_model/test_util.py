fad1a50d8bc40c2fdf6aba8bff5aa60541a3df85
Jonathon Belotti <jbelotti@zendesk.com>
2017-07-09 14:09:07 +1000

    create folder for linear models. move tests into 'models' folder of their own

---
+++ tests/models/nn_model/test_util.py
@@ -0,0 +1,41 @@
+from insults.models.nn_model.util import strip_html, LossHistory
+
+
+class TestLossHistory():
+
+    def setup_method(self, method):
+        self.loss_history = LossHistory()
+
+    def test_on_train_begin(self):
+        self.loss_history.on_train_begin()
+
+        assert [] == self.loss_history.losses
+        assert [] == self.loss_history.accuracies
+
+    def test_on_batch_end(self):
+        test_logs = {
+            'loss': [1, 2, 3, 4, 5],
+            'acc': [1, 2, 3, 4, 5]
+        }
+
+        self.loss_history.on_train_begin()
+        self.loss_history.on_batch_end(None, test_logs)
+
+        assert test_logs['loss'] == self.loss_history.losses[0]
+        assert test_logs['acc'] == self.loss_history.accuracies[0]
+
+        test_logs_second_batch = {
+            'loss': [5, 4, 3, 2, 1],
+            'acc': [5, 4, 3, 2, 1]
+        }
+
+        self.loss_history.on_batch_end(None, test_logs_second_batch)
+
+        assert test_logs_second_batch['loss'] == self.loss_history.losses[1]
+        assert test_logs_second_batch['acc'] == self.loss_history.accuracies[1]
+
+
+def test_strip_html():
+    test = "<p>This is a basic test string<br/> with html</p>"
+
+    assert "This is a basic test string with html" == strip_html(test)
